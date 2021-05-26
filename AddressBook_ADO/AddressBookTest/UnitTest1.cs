7677f9de7341b60c65cc6a32201496b3239ba286
Arjunsing1997 <acchur1997@gmail.com>
2021-05-26 17:47:53 +0530

    UC1 Address Book  data base Linked with C#

---
+++ AddressBook_ADO/AddressBookTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using AddressBook_ADO;
+namespace AddressBookTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddressBook_Return_TRUE()
+        {
+            AddressBookRepository addressBook = new AddressBookRepository();
+            bool Actual = addressBook.GetAllEmployee();
+            bool Exp = true;
+
+            Assert.AreEqual(Exp, Actual);
+
+        }
+    }
+}
